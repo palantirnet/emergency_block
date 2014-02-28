3c2b43222c9295f90c13ad10796da77e19514935
Larry Garfield <larry@garfieldtech.com>
2014-02-28 17:54:33 -0600

    Make violator block and state form work.

--- lib/Drupal/violator_block/Form/ViolatorForm.php
+++ lib/Drupal/violator_block/Form/ViolatorForm.php
@@ -1,27 +1,76 @@
 <?php
 
-namespace Drupal\violator_block;
+namespace Drupal\violator_block\Form;
 
 use Drupal\Core\Form\FormBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\KeyValueStore\StateInterface;
 
 /**
- * Description of ViolatorForm
- *
- * @author garfield
+ * Configuration form for the violator status.
  */
 class ViolatorForm extends FormBase {
 
-  // @todo this needs the state system, since it saves to that rather than config.
+  /**
+   * The state API store.
+   *
+   * @var \Drupal\Core\KeyValueStore\StateInterface
+   */
+  protected $state;
 
+  /**
+   * Creates a new ViolatorForm.
+   *
+   * @param \Drupal\Core\KeyValueStore\StateInterface $state
+   *   The state service.
+   */
+  public function __construct(StateInterface $state) {
+    $this->state = $state;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static($container->get('state'));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function getFormId() {
     return 'violator_block_form';
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function buildForm(array $form, array &$form_state) {
+    $form['status'] = [
+      '#type' => 'checkbox',
+      '#title' => $this->t('Show violator'),
+      '#default_value' => $this->state->get('violator_block.status'),
+    ];
 
+    $form['message'] = array(
+      '#type' => 'textarea',
+      '#title' => $this->t('Emergency message'),
+      '#default_value' => $this->state->get('violator_block.message'),
+    );
+
+    $form['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Save'),
+    ];
+
+    return $form;
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function submitForm(array &$form, array &$form_state) {
-
+    $this->state->set('violator_block.status', $form_state['values']['status']);
+    $this->state->set('violator_block.message', $form_state['values']['message']);
   }
 }
--- lib/Drupal/violator_block/Plugin/Block/EmergencyViolatorBlock.php
+++ lib/Drupal/violator_block/Plugin/Block/EmergencyViolatorBlock.php
@@ -1,24 +1,62 @@
 <?php
 
-namespace Drupal\violator_block;
+namespace Drupal\violator_block\Plugin\Block;
 
 use Drupal\block\BlockBase;
-use Drupal\Core\Session\AccountInterface;;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\Core\KeyValueStore\StateInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 
 /**
- * Description of EmergencyViolatorBlock
+ * Provides an emergency violator block.
  *
- * @author garfield
+ * @Block(
+ *   id = "violator_block_message",
+ *   admin_label = @Translation("Emergency violator"),
+ *   category = @Translation("System")
+ * )
  */
-class EmergencyViolatorBlock extends BlockBase {
+class EmergencyViolatorBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The state API store.
+   *
+   * @var \Drupal\Core\KeyValueStore\StateInterface
+   */
+  protected $state;
+
+  /**
+   * Constructs a new EmergencyViolatorBlock.
+   *
+   * @param array $configuration
+   * @param type $plugin_id
+   * @param array $plugin_definition
+   * @param \Drupal\Core\KeyValueStore\StateInterface $state
+   *   The state service.
+   */
+  public function __construct(array $configuration, $plugin_id, array $plugin_definition, StateInterface $state) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->state = $state;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, array $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('state')
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
   public function access(AccountInterface $account) {
-    // Replace with special visibilty logic based on the weather service and
-    // the configured state.
-    return TRUE;
+    return $this->state->get('violator_block.status');
   }
 
   /**
@@ -30,7 +68,9 @@ class EmergencyViolatorBlock extends BlockBase {
    * @see \Drupal\block\BlockViewBuilder
    */
   public function build() {
-    return 'Hello world';
+    return [
+      '#markup' => $this->state->get('violator_block.message'),
+    ];
   }
 
 }
