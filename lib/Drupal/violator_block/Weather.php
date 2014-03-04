d245d8c11153d5fc62dbad99631512e20f38d583
Larry Garfield <garfield@palantir.net>
2014-03-04 15:47:21 -0600

    Fix bug on zero-temp.

--- lib/Drupal/violator_block/Weather.php
+++ lib/Drupal/violator_block/Weather.php
@@ -76,7 +76,7 @@ class Weather {
     $temp = $this->state->get('violator_block.weather.temp');
     // We're not setting a default, so the default default is NULL. In that case
     // it's not too cold.
-    if ($temp && $temp <= $this->config->get('threshold')) {
+    if (!is_null($temp) && $temp <= $this->config->get('threshold')) {
       return TRUE;
     }
     return FALSE;
